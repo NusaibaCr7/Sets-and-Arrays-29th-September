23b7f2c1639d572e708ebe6c41e82dffc3b0f7ed
Nusaiba Faiza <nusaiba.faiza@gmail.com>
2024-09-29 18:18:55 +0600

    first commit

---
+++ Sets.py
@@ -0,0 +1,15 @@
+mySet = {1,2,3,3,3,2,2,}
+print(mySet)
+
+mySet.add("Nabila")
+mySet.add("Rose")
+mySet.add("Nusaiba")
+print(mySet)
+
+mySet.pop()
+mySet.pop()
+print(mySet)
+
+l1 = ["x","y","z","x"]
+newSet = set(l1)
+print(newSet)
