dc5873ec443980c0a3d5c490c2a6c8b55280cf8e
Nusaiba Faiza <nusaiba.faiza@gmail.com>
2024-10-03 16:52:46 +0600

    INFINITE AURAAA

---
+++ Set symmetric difference.py
@@ -0,0 +1,10 @@
+set1 = {'blue','green'}
+set2 = {'blue','yellow'}
+set3 = {1,2,3,4,5}
+set4 = {1,5,6,7,8,9}
+
+print("Symmetric Differences of each two sets: ")
+diff1 = set1.symmetric_difference(set2)
+diff2 = set3.symmetric_difference(set4)
+print(diff1)
+print(diff2)
---
+++ pseudocode.py
@@ -0,0 +1,8 @@
+original_price = 500
+discount_percentage = 20 
+
+discount_amount = original_price*(discount_percentage/100)
+
+res = original_price - discount_amount
+
+print(f"The payment price after Discount will be: {res} Taka")
